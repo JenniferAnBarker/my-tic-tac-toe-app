b5fb60bfcedd81df3f34d4462ac4c1c0a5d2fb9e
Jennifer N-B <Jenniebarker1o1@gmail.com>
2022-11-21 22:18:41 +0000

    lets get this deployed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,83 @@
+import { React, useState } from "react";
+import { Board } from "./Components/Board";
+import { ScoreBoard } from "./Components/ScoreBoard";
+import { ResetButton } from "./Components/ResetButton";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Please work</div>;
-}
+export default function App() {
+  let winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-export default App;
+  let [board, setBoard] = useState(Array(9).fill(null));
+  let [xPlaying, setXPlaying] = useState(true);
+  let [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  let [gameOver, setGameOver] = useState(false);
+
+  function handleBoxClick(boxIdx) {
+    let updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying === true ? "X" : "O";
+      } else {
+        return value;
+      }
+    });
+
+    let winner = checkWinner(updatedBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore });
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore });
+      }
+    }
+
+    setBoard(updatedBoard);
+
+    setXPlaying(!xPlaying);
+  }
+
+  let checkWinner = (board) => {
+    for (let i = 0; i < winConditions.length; i++) {
+      let [x, y, z] = winConditions[i];
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        setGameOver(true);
+        return board[x];
+      }
+    }
+  };
+  function resetBoard() {
+    setGameOver(false);
+    setBoard(Array(9).fill(null));
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="title">Let's play Tic-Tac-Toe!</h1>
+          <ScoreBoard scores={scores} xPlaying={xPlaying} />
+          <Board
+            board={board}
+            onClick={gameOver ? resetBoard : handleBoxClick}
+          />
+          <ResetButton resetBoard={resetBoard} />
+        </div>
+      </header>
+    </div>
+  );
+}
